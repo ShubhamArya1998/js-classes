e229cfcf0e1efb2580a19b02560fa8ea84c1603e
ShubhamArya1998 <97939807+ShubhamArya1998@users.noreply.github.com>
2024-04-30 06:18:59 +0000

    variables

---
+++ .devcontainer/01_basics/01-variables.js
@@ -0,0 +1,8 @@
+let accountName="shubham"
+const accountid=123
+accountCity="indore"
+let accountEmail;
+let accountState="MP"
+console.log(accountState)
+//using console.table to print values of variable together in table form instead of individual
+console.table([accountName,accountid,accountCity,accountEmail]);
