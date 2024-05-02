eea052fa9b7e6dc4e8adcf6f6db7b440aabba8c7
ShubhamArya1998 <97939807+ShubhamArya1998@users.noreply.github.com>
2024-05-02 07:47:59 +0000

    comparison of null

--- .devcontainer/01_basics/04_comparison.js
+++ .devcontainer/01_basics/04_comparison.js
@@ -9,8 +9,8 @@
 // console.log("02" > 1);
 
 console.log(null > 0);
-console.log(null == 0);
-console.log(null >= 0);
+console.log(null == 0); //In JavaScript, null is loosely equal (==) only to undefined and not to any other value. So the second condition, console.log(null == 0);, will return false because null is not loosely equal to 0.
+console.log(null >= 0); //when null is used in a numerical context, it is treated as 0. This includes comparisons using the greater than or equal to (>=) operator. 
 
 console.log(undefined == 0);
 console.log(undefined > 0);
