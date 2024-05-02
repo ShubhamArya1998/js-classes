06b4348502ffe051c2f01db66ad2e892ba3944c7
ShubhamArya1998 <97939807+ShubhamArya1998@users.noreply.github.com>
2024-05-02 07:37:49 +0000

    conversion of datatype

--- .devcontainer/01_basics/03_conversionOfDatatypes.js
+++ .devcontainer/01_basics/03_conversionOfDatatypes.js
@@ -17,3 +17,56 @@ console.log(randomvar);
 console.log(typeof randomvar) //datatype of null is object . null is its own type, distinct from objects, but historically it was incorrectly labeled as an object due to implementation decisions made in early JavaScript versions.
 let undefinedvar=undefined
 console.log(typeof undefinedvar) //datatype of undefined var is undefined itself
+
+let isLoggedIn = "shubham"
+
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+ console.log(booleanIsLoggedIn); //Result will be true because any non-empty string, regardless of its content, is considered "truthy" in JavaScript.
+
+ let boll=""
+ let boole=Boolean(boll)
+console.log(boole); //When you convert an empty string to a boolean value in most programming languages, the result will be false. This is because an empty string represents a lack of content or absence of characters, which typically corresponds to a "false" state in boolean logic.
+
+// 1 => true; 0 => false
+// "" => false
+// "shubham" => true
+
+//***********operations***************//
+
+let value = 3
+let negValue = -value
+// console.log(negValue);
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2**3);
+// console.log(2/3);
+// console.log(2%3);
+
+let str1 = "hello"
+let str2 = " hitesh"
+
+let str3 = str1 + str2
+// console.log(str3);
+
+// console.log("1" + 2); //output 12
+// console.log(1 + "2"); //output 12
+// console.log("1" + 2 + 2); //output 122 if string is at 1st then it will perform arthimetic operation it will consider whole statement as string
+ // console.log(1 + 2 + "2"); //output 32  if string is at last then it will perform arthimetic operation and then content of string will be return as it is at last
+
+ // console.log(1 + 2 + "abc"); //output 3abc  if string is at last then it will perform arthimetic operation and then content of string will be return as it is at last
+ 
+
+console.log( (3 + 4) * 5 % 3);
+
+// console.log(+true);
+// console.log(+"");
+
+/*let num1, num2, num3
+
+num1 = num2 = num3 = 2 + 2
+
+let gameCounter = 100
+++gameCounter;
+console.log(gameCounter);*/
