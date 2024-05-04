1ab990b9c562d3bf50ef30cd334497a5c1319d6f
ShubhamArya1998 <97939807+ShubhamArya1998@users.noreply.github.com>
2024-05-04 07:15:56 +0000

    datatypes_summary

--- .devcontainer/01_basics/Datatypes_summary.js
+++ .devcontainer/01_basics/Datatypes_summary.js
@@ -1,21 +1,28 @@
 //primitive data types
 // Symbol,String,boolean,bigInt,Number,Null,Undefined
+
+
 const id= Symbol ('123') //Symbol is used to create a totally unique, one-of-a-kind identifier.Even if you call Symbol with the same string, the instances will be different.
 const anotherId= Symbol('123')
 console.log(id===anotherId) //both are not equal
 console.log(typeof anotherId);
 
 let stringExample="ABC231"  //STRING EXAMPLE
+
+
 let IsLoggedIn=false //BOOLEAN EXAMPLE if we are login someuser and it is not available it will print false
 let outsideTemp=null // NULL EXAMPLE we are give outside temperature null it means it will be empty not 0
 console.log(outsideTemp); //it will print null
 
+
 let bigNumber=2124545451215454454545454545454545454n
 console.log(typeof bigNumber);
+
 
 const num1=50 //normal number
 const num2=50.50 //decimal number
 console.log(num1,num2);
+
 
 let undefinedExample // undefined can be code like this
 let undefinedExample1=undefined //undefined can be code like this also 
